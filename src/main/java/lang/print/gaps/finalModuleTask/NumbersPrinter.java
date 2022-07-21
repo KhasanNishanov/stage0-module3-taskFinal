4d3cc28c9b0f252b99a40501ef8da245d789ab10
Nishanov Khasan <99598107+HasanNishanov@users.noreply.github.com>
2022-07-21 08:31:35 +0500

    Good Morning!

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,25 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter { //3
     public void printIsPositive(int number) {
-        boolean check;
-         if(number>0 && number<=99){
-             check = true;
-             System.out.println(check);
-         }
-         else if(number<=0){
-             check = false;
-             System.out.println(check);
-         }
-         else {
-             check = false;
-             System.out.println(check);
-         }
-
+        boolean b = number > 0 && number <= 99;
+        System.out.println(b);
 
     }
 
     public static void main(String[] args) {
         NumbersPrinter numbersPrinter = new NumbersPrinter();
-        numbersPrinter.printIsPositive(0);
+        numbersPrinter.printIsPositive(100);
     }
 }
